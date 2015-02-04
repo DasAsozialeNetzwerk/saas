ecd84c7e766f7bd2465e13c2aa286ecf7a84990d
kohlrabi <kohlrabi@aragonit>
2015-02-04 22:04:15 +0100

    fix #1

--- Main.hs
+++ Main.hs
@@ -13,6 +13,14 @@ onMessage server msg
    | B.pack "%rms" `B.isPrefixOf` mMsg msg = do
         r <- (sample . choice . lines) =<< readFile "puns.txt"
         sendMsg server chan (B.pack r)
+   | B.pack "Amazon" `B.isInfixOf` mMsg msg = sendMsg server chan (B.append nick ": https://stallman.org/amazon.html")
+   | B.pack "Skype" `B.isInfixOf` mMsg msg = sendMsg server chan (B.append nick ": https://stallman.org/skype.html")
+   | B.pack "Google" `B.isInfixOf` mMsg msg = sendMsg server chan (B.append nick ": https://stallman.org/google.html")
+   | B.pack "Apple" `B.isInfixOf` mMsg msg = sendMsg server chan (B.append nick ": https://stallman.org/apple.html")
+   | B.pack "Facebook" `B.isInfixOf` mMsg msg = sendMsg server chan (B.append nick ": https://stallman.org/facebook.html")
+   | B.pack "Uber" `B.isInfixOf` mMsg msg = sendMsg server chan (B.append nick ": https://stallman.org/uber.html")
+   | B.pack "Netflix" `B.isInfixOf` mMsg msg = sendMsg server chan (B.append nick ": https://stallman.org/netflix.html")
+   | B.pack "Harry Potter" `B.isInfixOf` mMsg msg = sendMsg server chan (B.append nick ": https://stallman.org/harry-potter.html")
    | B.pack " Linux" `B.isInfixOf` mMsg msg = do -- TODO: schoenere Loesung finden
         sendMsg server chan (B.append nick ": I'd just like to interject for a moment. What you\'re referring to as Linux, is in fact, GNU/Linux, or as I\'ve recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.")
         sendMsg server chan (B.append nick ": Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called \"Linux\", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project. There really is a Linux, and these people are using it, but it is just a part of the system they use.")
